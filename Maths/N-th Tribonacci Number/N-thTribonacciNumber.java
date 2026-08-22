acfdadba6215650fd1f7d392b2186c57ca18fac9
Priyesh Dubey <2k24.cs1m.2414195@gmail.com>
2026-08-22 19:46:35 +0530

    Create N-thTribonacciNumber.java

---
+++ Maths/N-th Tribonacci Number/N-thTribonacciNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n<=1){
+            return n;
+        }
+        if(n==2){
+            return n-1;
+        }
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        for(int i = 3;i<=n;i++){
+            int d = a+b+c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return c;
+    }
+}
