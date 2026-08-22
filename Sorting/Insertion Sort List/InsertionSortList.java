d2d89f8ef29a2e50cd336aeca80d0f7603579ee8
Priyesh Dubey <2k24.cs1m.2414195@gmail.com>
2026-08-23 01:32:19 +0530

    Create InsertionSortList.java

---
+++ Sorting/Insertion Sort List/InsertionSortList.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public ListNode insertionSortList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode sorted = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            ListNode next = curr.next;
+
+            if (sorted == null || curr.val < sorted.val) {
+                curr.next = sorted;
+                sorted = curr;
+            } 
+            else {
+                ListNode temp = sorted;
+
+                while (temp.next != null && temp.next.val <= curr.val) {
+                    temp = temp.next;
+                }
+
+                curr.next = temp.next;
+                temp.next = curr;
+            }
+
+            curr = next;
+        }
+
+        return sorted;
+    }
+}
