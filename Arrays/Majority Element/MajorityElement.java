94a70f5e1e69babd1236b7048ad0e5b171a52466
Priyesh Dubey <2k24.cs1m.2414195@gmail.com>
2026-09-05 10:02:34 +0530

    Update MajorityElement.java

--- Arrays/Majority Element/MajorityElement.java
+++ Arrays/Majority Element/MajorityElement.java
@@ -5,3 +5,23 @@ class Solution {
         return nums[n/2];
     }
 }
+
+
+// Another Approach for this problem is using Hashmap 
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        for(int x:nums){
+            freq.put(x,freq.getOrDefault(x,0)+1);
+        }
+        int ans = -1;
+        for(int x:freq.keySet()){
+            if(freq.get(x)>n/2){
+                ans = x;
+            }
+        }
+        return ans;
+    }
+}
