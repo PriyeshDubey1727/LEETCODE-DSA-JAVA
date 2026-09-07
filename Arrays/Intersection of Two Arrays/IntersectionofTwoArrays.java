aa0c9deb22055a8c679386216820327db1556397
Priyesh Dubey <2k24.cs1m.2414195@gmail.com>
2026-09-07 10:04:24 +0530

    Create IntersectionofTwoArrays.java

---
+++ Arrays/Intersection of Two Arrays/IntersectionofTwoArrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int x:nums1){
+            set.add(x);
+        }
+        HashSet<Integer> result = new HashSet<>();
+        for(int x:nums2){
+            if(set.contains(x)){
+                result.add(x);
+            }
+        }
+        int[] ans = new int[result.size()];
+        int i = 0;
+        for(int x:result){
+            ans[i] = x;
+            i++;
+        }
+        return ans;
+    }
+}
