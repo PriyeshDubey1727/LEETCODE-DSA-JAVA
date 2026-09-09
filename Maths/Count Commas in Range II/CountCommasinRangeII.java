48d0a45435ca0e0cbeac991105e950cd35eb6c48
Priyesh Dubey <2k24.cs1m.2414195@gmail.com>
2026-09-09 11:12:01 +0530

    Create CountCommasinRangeII.java

---
+++ Maths/Count Commas in Range II/CountCommasinRangeII.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        for(long x = 1000;x<=n;){
+            ans = ans+ n-x+1;
+            if(x>n/1000){
+                break;
+            }
+            x*=1000;
+        }
+        return ans;
+    }
+}
