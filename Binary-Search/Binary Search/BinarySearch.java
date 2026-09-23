38e55a9cd0d5657133cda3a99171aad134543375
Priyesh Dubey <2k24.cs1m.2414195@gmail.com>
2026-09-23 16:15:40 +0530

    Update BinarySearch.java

--- Binary-Search/Binary Search/BinarySearch.java
+++ Binary-Search/Binary Search/BinarySearch.java
@@ -1,21 +1,55 @@
+// BINARY SEARCH IMPLEMENTATION
+// class Solution {
+//     public int search(int[] arr, int target){
+//         int n = arr.length;
+//         int s = 0;
+//         int e = n-1;
+//         while(s<=e){
+//             int m = s+(e-s)/2;
+//             if(arr[m]==target){
+//                 return m;
+//             }
+//             else if(arr[m]<target){
+//                 s = m+1;
+//             }
+//             else{
+//                 e = m-1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+ Time Complexity = O(logn);
+ Space Complexity = O(1);
+
+
+
+// RECURSIVE BINARY SEARCH IMPLEMENTATION
 class Solution {
-    public int search(int[] arr, int target) {
+    public int search(int[] arr, int target){
         int n = arr.length;
-        int start = 0;
-        int end = n-1;
-        int mid = (start+end)/2;
-        while(start<=end){
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]<target){
-                start = mid+1;
-            }
-            else{
-                end = mid-1;
-            }
-            mid = (start+end)/2;
+        int s = 0;
+        int e = n-1;
+        return binarySearch(arr,target, s , e);
+    }
+    static int binarySearch(int[] arr, int target, int s, int e){
+        //base case
+        if(s>e){
+            return -1;
         }
-        return -1;
+        //processing work 
+        int mid = s+(e-s)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]<target){
+            s = mid+1;
+        }
+        else{
+            e = mid-1;
+        }
+        //recursive call
+        return binarySearch(arr, target, s, e);
     }
 }
